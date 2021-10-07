06014baad33d166b625dd1dad39d57af27710639
osama-khan <osama6756@gmail.com>
2021-10-07 20:38:10 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View style={{height: '100%', alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 48, fontWeight: '100'}}>OBE</Text>
+    </View>
+  );
+};
+
+export default App;
