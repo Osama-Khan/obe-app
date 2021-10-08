580824c61eb87da030d54498268f7bbe22d3a0ca
Osama Khan <Osama6756@gmail.com>
2021-10-08 18:51:58 +0500

    Updated Home UI

--- src/app/screens/home.tsx
+++ src/app/screens/home.tsx
@@ -1,22 +1,25 @@
 import {Header} from '@app/components/header';
 import React from 'react';
 import {ScrollView} from 'react-native';
-import {Button, Title} from 'react-native-paper';
+import {List} from 'react-native-paper';
 
 export const Home = () => (
   <>
     <Header />
-    <ScrollView>
-      <Title>Courses</Title>
-      <Button onPress={() => {}}>Add</Button>
-      <Button onPress={() => {}}>View</Button>
-      <Button onPress={() => {}}>Allocate</Button>
-      <Title>Programs</Title>
-      <Button onPress={() => {}}>Add</Button>
-      <Button onPress={() => {}}>View</Button>
-      <Title>Teachers</Title>
-      <Button onPress={() => {}}>Add</Button>
-      <Button onPress={() => {}}>View</Button>
+    <ScrollView style={{paddingHorizontal: 8}}>
+      <List.Section title="Courses">
+        <List.Item title="Add" left={() => <List.Icon icon="plus" />} />
+        <List.Item title="View" left={() => <List.Icon icon="eye" />} />
+        <List.Item title="Allocate" left={() => <List.Icon icon="sitemap" />} />
+      </List.Section>
+      <List.Section title="Programs">
+        <List.Item title="Add" left={() => <List.Icon icon="plus" />} />
+        <List.Item title="View" left={() => <List.Icon icon="eye" />} />
+      </List.Section>
+      <List.Section title="Teachers">
+        <List.Item title="Add" left={() => <List.Icon icon="plus" />} />
+        <List.Item title="View" left={() => <List.Icon icon="eye" />} />
+      </List.Section>
     </ScrollView>
   </>
 );
