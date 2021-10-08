a6fd17b31700646a216dc248d9e5e873501619de
Osama Khan <Osama6756@gmail.com>
2021-10-08 19:23:53 +0500

    Implemented AddCourseScreen

--- src/app/screens/courses/add-course.screen.tsx
+++ src/app/screens/courses/add-course.screen.tsx
@@ -1,9 +1,49 @@
-import {Header} from '@app/components/header';
+import {Formik} from 'formik';
 import React from 'react';
 import {View} from 'react-native';
+import {Button, Card, TextInput} from 'react-native-paper';
 
 export const AddCourseScreen = () => (
-  <View>
-    <Header />
+  <View style={{height: '100%'}}>
+    <Formik
+      initialValues={{
+        name: '',
+        program: '',
+      }}
+      onSubmit={v => {
+        console.log(v);
+      }}
+      children={({handleChange, handleSubmit, values}) => (
+        <Card
+          style={{
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            padding: 8,
+            marginHorizontal: 8,
+          }}>
+          <TextInput
+            label="Name"
+            mode="outlined"
+            value={values.name}
+            onChangeText={handleChange('name')}
+            style={{marginVertical: 4}}
+          />
+          <TextInput
+            label="Program"
+            mode="outlined"
+            value={values.program}
+            onChangeText={handleChange('program')}
+            style={{marginVertical: 4}}
+          />
+          <Button
+            mode="contained"
+            icon="check"
+            style={{marginVertical: 4, marginLeft: 'auto'}}
+            onPress={handleSubmit}>
+            Submit
+          </Button>
+        </Card>
+      )}
+    />
   </View>
 );
--- src/app/screens/home.tsx
+++ src/app/screens/home.tsx
@@ -1,14 +1,21 @@
 import {Header} from '@app/components/header';
+import {NavigationProp} from '@react-navigation/core';
 import React from 'react';
 import {ScrollView} from 'react-native';
 import {List} from 'react-native-paper';
+import {addCourseRoute} from 'src/app.routes';
 
-export const Home = () => (
+type P = {navigation: NavigationProp<any>};
+export const Home = (props: P) => (
   <>
     <Header />
     <ScrollView style={{paddingHorizontal: 8}}>
       <List.Section title="Courses">
-        <List.Item title="Add" left={() => <List.Icon icon="plus" />} />
+        <List.Item
+          title="Add"
+          left={() => <List.Icon icon="plus" />}
+          onPress={() => props.navigation.navigate(addCourseRoute.name)}
+        />
         <List.Item title="View" left={() => <List.Icon icon="eye" />} />
         <List.Item title="Allocate" left={() => <List.Icon icon="sitemap" />} />
       </List.Section>
