f0717219884712a97b47fcf7e7425a9c9234b249
Osama Khan <Osama6756@gmail.com>
2021-10-08 18:39:19 +0500

    Added basic view in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import {StatusBar, View} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {homeRoute, routes} from './app.routes';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {routes} from './app.routes';
 
-const Stack = createStackNavigator();
-
+const {Navigator, Screen} = createStackNavigator();
 const App = () => {
   return (
-    <View style={{backgroundColor: '#eee', height: '100%'}}>
+    <NavigationContainer>
       <StatusBar backgroundColor="#50d" />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName={homeRoute.name}>
-          {routes.stackNav.map(r => (
-            <Stack.Screen component={r.component} name={r.name} key={r.id} />
-          ))}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+      <Navigator>
+        {routes.stackNav.map(r => (
+          <Screen
+            component={r.component}
+            name={r.name}
+            key={r.id}
+            options={r.options}
+          />
+        ))}
+      </Navigator>
+    </NavigationContainer>
   );
 };
 
