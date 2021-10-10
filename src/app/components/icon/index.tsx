377e70c829a100a37a426ecc108ae5246c09def1
osama-khan <osama6756@gmail.com>
2021-10-10 14:13:02 +0500

    Added Icon component

---
+++ src/app/components/icon/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {IconProps} from 'react-native-vector-icons/Icon';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function Icon(props: IconProps) {
+  return <MaterialIcon {...props} color={props.color} />;
+}
