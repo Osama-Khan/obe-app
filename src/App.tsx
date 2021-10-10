8e7b97c36e765140ee82334242d7216bde1c8f8b
osama-khan <osama6756@gmail.com>
2021-10-10 19:39:53 +0500

    Added React Native Paper Provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {DefaultTheme, Provider} from 'react-native-paper';
 import {routes} from './app.routes';
 
 const {Navigator, Screen} = createStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="#50d" />
-      <Navigator>
-        {routes.stackNav.map(r => (
-          <Screen
-            component={r.component}
-            name={r.name}
-            key={r.id}
-            options={r.options}
-          />
-        ))}
-      </Navigator>
-    </NavigationContainer>
+    <Provider theme={DefaultTheme}>
+      <NavigationContainer>
+        <StatusBar backgroundColor="#50d" />
+        <Navigator>
+          {routes.stackNav.map(r => (
+            <Screen
+              component={r.component}
+              name={r.name}
+              key={r.id}
+              options={r.options}
+            />
+          ))}
+        </Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
