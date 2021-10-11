583df5f5742dcc3b253d054865678e4b5eee2843
osama-khan <osama6756@gmail.com>
2021-10-11 15:20:07 +0500

    Added login method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,14 @@
 import UserType from '@app/types/user.type';
+import axios from 'axios';
 import CrudService from './crud.service';
 
 class UserService extends CrudService<UserType> {
   constructor() {
     super('user');
+  }
+
+  login(data: {username: string; password: string}) {
+    return axios.post(this.endpoint + '/login', data);
   }
 }
 
