6de5dd2cf0771f31e4bf88ff3e1aa7d3a45b1213
osama-khan <osama6756@gmail.com>
2021-10-12 12:30:36 +0500

    Added ReduxProvider and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {Provider as ReduxProvider} from 'react-redux';
 import {StatusBar} from 'react-native';
 import {DefaultTheme, Provider} from 'react-native-paper';
 import {routes} from './app.routes';
+import store from '@app/store';
 
 const {Navigator, Screen} = createStackNavigator();
 const App = () => {
   return (
-    <Provider theme={DefaultTheme}>
-      <NavigationContainer>
-        <StatusBar backgroundColor="#50d" />
-        <Navigator>
-          {routes.stackNav.map(r => (
-            <Screen
-              component={r.component}
-              name={r.name}
-              key={r.id}
-              options={r.options}
-            />
-          ))}
-        </Navigator>
-      </NavigationContainer>
-    </Provider>
+    <ReduxProvider store={store}>
+      <Provider theme={DefaultTheme}>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#50d" />
+          <Navigator>
+            {routes.stackNav.map(r => (
+              <Screen
+                component={r.component}
+                name={r.name}
+                key={r.id}
+                options={r.options}
+              />
+            ))}
+          </Navigator>
+        </NavigationContainer>
+      </Provider>
+    </ReduxProvider>
   );
 };
 
