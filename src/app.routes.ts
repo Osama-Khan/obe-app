1f516a68a48bf4922982b875b6f31c264df4b76d
osama-khan <osama6756@gmail.com>
2021-10-13 15:23:59 +0500

    Added header on LoginScreen

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -26,7 +26,6 @@ export const loginRoute: RouteType = {
   id: 'login',
   name: 'Login',
   component: LoginScreen,
-  options: {headerShown: false},
 };
 export const homeRoute: RouteType = {
   id: 'home',
