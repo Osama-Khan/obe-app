74173d917ee887289dfa148bcf934458703491b7
osama-khan <osama6756@gmail.com>
2021-10-14 21:36:54 +0500

    Improved home UI

--- src/app/screens/home.tsx
+++ src/app/screens/home.tsx
@@ -1,59 +1,61 @@
-import {Header} from '@app/components/header';
 import {NavigationProp} from '@react-navigation/core';
-import React from 'react';
-import {ScrollView} from 'react-native';
-import {List} from 'react-native-paper';
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import {Card, Title} from 'react-native-paper';
 import {
-  addCourseRoute,
-  addProgramRoute,
-  addUserRoute,
   viewCourseRoute,
   viewProgramRoute,
   viewUsersRoute,
 } from 'src/app.routes';
+import Icon from '@app/components/icon';
 
 type P = {navigation: NavigationProp<any>};
-export const Home = (props: P) => (
-  <>
-    <Header />
-    <ScrollView style={{paddingHorizontal: 8}}>
-      <List.Section title="Courses">
-        <List.Item
-          title="Add"
-          left={() => <List.Icon icon="plus" />}
-          onPress={() => props.navigation.navigate(addCourseRoute.name)}
-        />
-        <List.Item
-          title="View"
-          left={() => <List.Icon icon="eye" />}
-          onPress={() => props.navigation.navigate(viewCourseRoute.name)}
-        />
-        <List.Item title="Allocate" left={() => <List.Icon icon="sitemap" />} />
-      </List.Section>
-      <List.Section title="Programs">
-        <List.Item
-          title="Add"
-          left={() => <List.Icon icon="plus" />}
-          onPress={() => props.navigation.navigate(addProgramRoute.name)}
-        />
-        <List.Item
-          title="View"
-          left={() => <List.Icon icon="eye" />}
-          onPress={() => props.navigation.navigate(viewProgramRoute.name)}
-        />
-      </List.Section>
-      <List.Section title="Teachers">
-        <List.Item
-          title="Add"
-          left={() => <List.Icon icon="plus" />}
-          onPress={() => props.navigation.navigate(addUserRoute.name)}
-        />
-        <List.Item
-          title="View"
-          left={() => <List.Icon icon="eye" />}
-          onPress={() => props.navigation.navigate(viewUsersRoute.name)}
-        />
-      </List.Section>
-    </ScrollView>
-  </>
+export const Home = (props: P) => {
+  useEffect(() => {
+    props.navigation.setOptions({
+      headerTitleAlign: 'center',
+      headerTitleAllowFontScaling: true,
+    });
+  }, []);
+  const goto = (r: string) => props.navigation.navigate(r);
+  return (
+    <>
+      <IconCard
+        icon="book"
+        title="Courses"
+        onPress={() => goto(viewCourseRoute.name)}
+      />
+      <IconCard
+        icon="folder"
+        title="Programs"
+        onPress={() => goto(viewProgramRoute.name)}
+      />
+      <IconCard
+        icon="account"
+        title="Users"
+        onPress={() => goto(viewUsersRoute.name)}
+      />
+    </>
+  );
+};
+
+const IconCard = ({
+  title,
+  icon,
+  onPress,
+}: {
+  title: string;
+  icon: string;
+  onPress: () => void;
+}) => (
+  <Card style={{margin: 8, overflow: 'hidden'}} elevation={8} onPress={onPress}>
+    <View style={{alignItems: 'center', flexDirection: 'row'}}>
+      <Icon
+        name={icon}
+        size={48}
+        style={{padding: 8, backgroundColor: '#70f'}}
+      />
+      <Title style={{marginLeft: 8}}>{title}</Title>
+    </View>
+  </Card>
 );
