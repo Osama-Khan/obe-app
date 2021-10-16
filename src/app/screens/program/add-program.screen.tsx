4a89d2d09bc8c720dbaf15b93172734e7ef6b2e3
osama-khan <osama6756@gmail.com>
2021-10-16 16:47:45 +0500

    Added PLO addition

--- src/app/screens/program/add-program.screen.tsx
+++ src/app/screens/program/add-program.screen.tsx
@@ -1,44 +1,115 @@
 import programService from '@app/services/program.service';
 import ProgramType from '@app/types/program.type';
 import {Formik} from 'formik';
-import React from 'react';
-import {ToastAndroid, View} from 'react-native';
-import {Button, Card, TextInput} from 'react-native-paper';
+import React, {useState} from 'react';
+import {ToastAndroid, ScrollView, View} from 'react-native';
+import {
+  Button,
+  Caption,
+  Card,
+  IconButton,
+  TextInput,
+  Title,
+} from 'react-native-paper';
 
-export const AddProgramScreen = () => (
-  <View style={{height: '100%'}}>
-    <Formik
-      initialValues={{
-        title: '',
-      }}
-      onSubmit={handleSubmit}
-      children={({handleChange, handleSubmit, values}) => (
-        <Card
-          style={{
-            marginTop: 'auto',
-            marginBottom: 'auto',
-            padding: 8,
-            marginHorizontal: 8,
-          }}>
-          <TextInput
-            label="Title"
+export const AddProgramScreen = () => {
+  const [plos, setPlos] = useState([{title: '', description: ''}]);
+  const [title, setTitle] = useState('');
+  return (
+    <ScrollView>
+      <View style={{marginTop: 8}} />
+      <Card
+        style={{
+          marginTop: 'auto',
+          marginBottom: 'auto',
+          padding: 8,
+          marginHorizontal: 8,
+        }}>
+        <TextInput
+          label="Title"
+          mode="outlined"
+          value={title}
+          onChangeText={setTitle}
+          style={{marginVertical: 4}}
+        />
+        <Title>PLOs</Title>
+        {plos.map((plo, i) => (
+          <Card
+            key={i}
             mode="outlined"
-            value={values.title}
-            onChangeText={handleChange('title')}
-            style={{marginVertical: 4}}
-          />
-          <Button
-            mode="contained"
-            icon="check"
-            style={{marginVertical: 4, marginLeft: 'auto'}}
-            onPress={handleSubmit}>
-            Submit
-          </Button>
-        </Card>
-      )}
-    />
-  </View>
-);
+            style={{
+              margin: 4,
+              marginVertical: 12,
+              padding: 12,
+              overflow: 'visible',
+            }}>
+            <View
+              style={{
+                position: 'absolute',
+                top: -26,
+                flexDirection: 'row',
+                width: '100%',
+              }}>
+              <Caption style={{paddingHorizontal: 4, backgroundColor: 'white'}}>
+                {`PLO #${i + 1}`}
+              </Caption>
+              <IconButton
+                style={{
+                  margin: 0,
+                  marginLeft: 'auto',
+                  backgroundColor: 'white',
+                }}
+                color="gray"
+                size={18}
+                icon="close"
+                onPress={() => {
+                  setPlos(plos.filter((_, ind) => ind !== i));
+                }}
+              />
+            </View>
+            <TextInput
+              value={plo.title}
+              onChangeText={title => {
+                plos[i].title = title;
+                setPlos([...plos]);
+              }}
+              mode="outlined"
+              placeholder="Title"
+              dense
+            />
+            <TextInput
+              value={plo.description}
+              onChangeText={description => {
+                plos[i].description = description;
+                setPlos([...plos]);
+              }}
+              mode="outlined"
+              placeholder="Description"
+              dense
+              multiline
+            />
+          </Card>
+        ))}
+        <Button
+          icon="plus"
+          onPress={() => {
+            const p = plos.concat({title: '', description: ''});
+            setPlos(p);
+          }}>
+          Add PLO
+        </Button>
+        <Button
+          mode="contained"
+          icon="check"
+          disabled={isInvalid({title})}
+          style={{marginVertical: 4, marginLeft: 'auto'}}
+          onPress={() => handleSubmit({title, plos})}>
+          Submit
+        </Button>
+      </Card>
+    </ScrollView>
+  );
+};
 
 const isInvalid = (data: Partial<ProgramType>) => !data.title;
 const handleSubmit = (data: Partial<ProgramType>) => {
