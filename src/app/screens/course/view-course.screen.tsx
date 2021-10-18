ae6c28468e31a6cbd68a22e1b06b9c716cc9d919
osama-khan <osama6756@gmail.com>
2021-10-18 21:36:17 +0500

    Added deletion on Course and Program screens

--- src/app/screens/course/view-course.screen.tsx
+++ src/app/screens/course/view-course.screen.tsx
@@ -1,30 +1,66 @@
 import {FetchingCardList} from '@app/components/listing';
+import {ConfirmModal} from '@app/components/modal';
 import courseService from '@app/services/course.service';
 import CourseType from '@app/types/course.type';
 import {NavigationProp} from '@react-navigation/core';
-import React from 'react';
-import {Button} from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import {ToastAndroid} from 'react-native';
+import {IconButton} from 'react-native-paper';
 import {addCourseRoute} from 'src/app.routes';
 
 type P = {navigation: NavigationProp<any>};
 export const ViewCourseScreen = ({navigation}: P) => {
+  const [selected, setSelected] = useState<CourseType>();
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <IconButton
+          style={{marginVertical: 'auto'}}
+          icon="plus-circle"
+          color="white"
+          onPress={() => navigation.navigate(addCourseRoute.name)}
+        />
+      ),
+    });
+  }, []);
   return (
     <>
-      <Button
-        icon="plus"
-        style={{margin: 8, marginLeft: 'auto'}}
-        mode="contained"
-        onPress={() => navigation.navigate(addCourseRoute.name)}>
-        Add
-      </Button>
       <FetchingCardList<CourseType>
         fetchMethod={() => courseService.get()}
-        description={item =>
-          item.programs
-            ? item.programs?.map(p => p.title).join(',')
-            : 'No Programs'
-        }
         title={item => item.title}
+        description={item => item.code}
+        handleDelete={item => {
+          setSelected(item);
+        }}
+      />
+      <ConfirmModal
+        title="Delete Course?"
+        description={`Are you sure you want to delete the course "${selected?.title}"?`}
+        visible={!!selected}
+        positiveButton={{
+          onPress: () => {
+            courseService
+              .delete(selected!.id)
+              .then(res => {
+                ToastAndroid.show(`Course deleted!`, ToastAndroid.LONG);
+              })
+              .catch(e => {
+                ToastAndroid.show(
+                  `Could not delete course!`,
+                  ToastAndroid.LONG,
+                );
+              });
+            setSelected(undefined);
+          },
+        }}
+        negativeButton={{
+          onPress: () => {
+            setSelected(undefined);
+          },
+        }}
+        onDismiss={() => {
+          setSelected(undefined);
+        }}
       />
     </>
   );
--- src/app/screens/program/view-program.screen.tsx
+++ src/app/screens/program/view-program.screen.tsx
@@ -1,13 +1,16 @@
 import {FetchingCardList} from '@app/components/listing';
+import {ConfirmModal} from '@app/components/modal';
 import programService from '@app/services/program.service';
 import ProgramType from '@app/types/program.type';
 import {NavigationProp} from '@react-navigation/core';
-import React from 'react';
+import React, {useState} from 'react';
+import {ToastAndroid} from 'react-native';
 import {Button} from 'react-native-paper';
 import {addProgramRoute} from 'src/app.routes';
 
 type P = {navigation: NavigationProp<any>};
 export const ViewProgramScreen = ({navigation}: P) => {
+  const [selected, setSelected] = useState<ProgramType>();
   return (
     <>
       <Button
@@ -21,6 +24,38 @@ export const ViewProgramScreen = ({navigation}: P) => {
         fetchMethod={() => programService.get()}
         title={item => item.title}
         description={item => item.id}
+        handleDelete={item => {
+          setSelected(item);
+        }}
+      />
+      <ConfirmModal
+        title="Delete Program?"
+        description={`Are you sure you want to delete the program "${selected?.title}"?`}
+        visible={!!selected}
+        positiveButton={{
+          onPress: () => {
+            programService
+              .delete(selected!.id)
+              .then(res => {
+                ToastAndroid.show(`Program deleted!`, ToastAndroid.LONG);
+              })
+              .catch(e => {
+                ToastAndroid.show(
+                  `Could not delete program!`,
+                  ToastAndroid.LONG,
+                );
+              });
+            setSelected(undefined);
+          },
+        }}
+        negativeButton={{
+          onPress: () => {
+            setSelected(undefined);
+          },
+        }}
+        onDismiss={() => {
+          setSelected(undefined);
+        }}
       />
     </>
   );
