007bd4c8dc09402d9def09733cf1641766c0359c
osama-khan <osama6756@gmail.com>
2021-10-19 16:07:31 +0500

    Changed domain to work with emulator by default

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 /** Provides basic API properties and methods */
 export default abstract class ApiService {
   /** Domain the API is hosted at */
-  domain = 'localhost:3001';
+  domain = 'http://10.0.2.2:3000';
 }
