848fa6a1a70b976b94635dcaebff7b357030fe6e
Osama Khan <Osama6756@gmail.com>
2021-11-04 13:47:44 +0500

    Delete header.tsx

--- src/app/components/header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-
-export const Header = () => (
-  <View style={{alignItems: 'center', backgroundColor: '#70f'}}>
-    <Text style={{fontSize: 48, color: 'white', fontWeight: '300'}}>OBE</Text>
-  </View>
-);
