25280071808f6d8f5ad33ca42664e4a961edf15e
Osama Khan <Osama6756@gmail.com>
2021-11-14 19:08:41 +0500

    Added CLO-PLO weight management

--- src/app/screens/hod/course/add-clo.view.tsx
+++ src/app/screens/hod/course/add-clo.view.tsx
@@ -1,16 +1,15 @@
-import ListSelect from '@app/components/list-select';
 import cloService from '@app/services/clo.service';
-import ploService from '@app/services/plo.service';
 import uiService from '@app/services/ui.service';
 import {colors} from '@app/styles';
 import {CLOType, PLOType} from '@app/types';
-import React, {useEffect, useState} from 'react';
-import {View, FlatList} from 'react-native';
+import ObjectiveMapType from '@app/types/objective-map.type';
+import React, {useState} from 'react';
+import {View, FlatList, ScrollView} from 'react-native';
 import {
   ActivityIndicator,
   Button,
-  Caption,
   Chip,
+  Divider,
   Text,
   TextInput,
   Title,
@@ -19,31 +18,43 @@ import {
 type P = {
   /** ID of the course the CLO is being added for */
   courseId: string;
+  /** PLOs available for the course */
+  plos: (PLOType & {weight: number})[];
   /** Called with the clo when a clo is successfully added */
   onAdd: (clo: Partial<CLOType>) => void;
 };
 
-export default function AddCloView({onAdd, courseId}: P) {
-  const [plos, setPlos] = useState<PLOType[]>();
-  const [added, setAdded] = useState<Partial<PLOType>[]>([]);
+export default function AddCloView({onAdd, courseId, plos}: P) {
+  const [added, setAdded] = useState<
+    Pick<ObjectiveMapType, 'plo' | 'weight'>[]
+  >([]);
   const [title, setTitle] = useState('');
+  const [weight, setWeight] = useState(25);
   const [desc, setDesc] = useState('');
   const [saving, setSaving] = useState(false);
 
-  const addPlo = (p: PLOType) => setAdded([...added, p]);
-  const removePlo = (id: string) => setAdded(added.filter(a => a.id !== id));
-
-  useEffect(() => {
-    ploService
-      .get()
-      .then(r => setPlos(r.data))
-      .catch(e => uiService.toastError('Could not fetch PLOs!'));
-  }, []);
+  const addPlo = (plo: PLOType & {weight: number}, weight: number) => {
+    if (weight <= 0) {
+      uiService.toastError(`Weight can't be 0`);
+      return;
+    }
+    if (weight > 100 || weight + plo.weight > 100) {
+      const possibleWeight = 100 - plo.weight;
+      uiService.toastError(
+        `Max possible weight is ${possibleWeight}%`,
+        'Too much weight!',
+      );
+      return;
+    }
+    setAdded([...added, {plo, weight}]);
+  };
+  const removePlo = (id: string) =>
+    setAdded(added.filter(a => a.plo?.id !== id));
 
   return (
-    <View>
-      <View style={{marginHorizontal: 16, marginTop: 16}}>
-        <Title>Add CLO</Title>
+    <ScrollView>
+      <View style={{marginHorizontal: 16}}>
+        <Title style={{textAlign: 'center'}}>Add CLO</Title>
         <TextInput
           mode="outlined"
           label="Title"
@@ -56,8 +67,30 @@ export default function AddCloView({onAdd, courseId}: P) {
           value={desc}
           onChangeText={setDesc}
         />
-        <Title>Map PLOs</Title>
-        <Caption>Tap to add PLO</Caption>
+        <Divider style={{marginVertical: 8}} />
+        <Title>PLO Mapping</Title>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text>Tap to add PLO with</Text>
+          <TextInput
+            value={weight.toString()}
+            style={{
+              backgroundColor: '#0000',
+              margin: 4,
+            }}
+            keyboardType="decimal-pad"
+            onChangeText={weight => {
+              if (weight === '') {
+                setWeight(1);
+                return;
+              }
+              const num = parseInt(weight);
+              if (num === NaN) return;
+              setWeight(num > 0 ? num : 1);
+            }}
+            dense
+          />
+          <Text>% weight</Text>
+        </View>
       </View>
       <FlatList
         horizontal
@@ -67,12 +100,13 @@ export default function AddCloView({onAdd, courseId}: P) {
         ListHeaderComponent={<View style={{width: 16}} />}
         ListFooterComponent={<View style={{width: 16}} />}
         renderItem={({item: p}) => {
-          const isAdded = added.find(a => p.id === a.id);
+          const isAdded = added.find(a => p.id === a.plo?.id);
           return (
             <Chip
-              icon={isAdded ? undefined : 'plus'}
-              onPress={isAdded ? undefined : () => addPlo(p)}
+              icon={isAdded || p.weight >= 100 ? undefined : 'plus'}
+              onPress={isAdded ? undefined : () => addPlo(p, weight)}
               onClose={isAdded ? () => removePlo(p.id) : undefined}
+              disabled={p.weight >= 100}
               style={[
                 {margin: 4},
                 isAdded
@@ -82,9 +116,12 @@ export default function AddCloView({onAdd, courseId}: P) {
                       borderWidth: 2,
                     }
                   : {},
+                p.weight >= 100 ? {opacity: 0.5} : {},
               ]}
               key={p.id}>
-              <Text>{p.title}</Text>
+              <Text>
+                {p.title} - {p.weight + (isAdded ? isAdded.weight : 0)}%
+              </Text>
             </Chip>
           );
         }}
@@ -92,18 +129,20 @@ export default function AddCloView({onAdd, courseId}: P) {
       />
       <Button
         style={{marginTop: 16, borderRadius: 0}}
-        icon="plus"
+        icon="check"
         mode="contained"
         disabled={!title || !desc || saving}
         loading={saving}
         onPress={() => {
+          setSaving(true);
+          const clo = {
+            title,
+            description: desc,
+            course: {id: courseId},
+            maps: added.map(m => ({plo: {id: m.plo!.id}, weight: m.weight})),
+          };
           cloService
-            .insert({
-              title,
-              description: desc,
-              course: {id: courseId},
-              plos: added.map(p => ({id: p.id})),
-            })
+            .insert(clo)
             .then(r => {
               onAdd(r.data);
             })
@@ -116,6 +155,6 @@ export default function AddCloView({onAdd, courseId}: P) {
         }}>
         Add
       </Button>
-    </View>
+    </ScrollView>
   );
 }
--- src/app/screens/hod/course/detail-course.screen.tsx
+++ src/app/screens/hod/course/detail-course.screen.tsx
@@ -1,31 +1,69 @@
-import {FetchingDataTable} from '@app/components/data-table';
+import {DataTable} from '@app/components/data-table';
 import Modal from '@app/components/modal';
 import {ManyCriteria} from '@app/models/criteria';
 import cloService from '@app/services/clo.service';
-import {CLOType, CourseType} from '@app/types';
+import ploService from '@app/services/plo.service';
+import uiService from '@app/services/ui.service';
+import {colors} from '@app/styles';
+import {CLOType, CourseType, PLOType} from '@app/types';
 import {useNavigation, useRoute} from '@react-navigation/core';
-import React, {useMemo, useState} from 'react';
-import {View} from 'react-native';
-import {Button, Card, IconButton, Text} from 'react-native-paper';
+import React, {useEffect, useMemo, useState} from 'react';
+import {ScrollView, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Button,
+  Caption,
+  Card,
+  Divider,
+  IconButton,
+  ProgressBar,
+  Text,
+  Title,
+} from 'react-native-paper';
 import AddCloView from './add-clo.view';
-
-let criteria: ManyCriteria<CLOType>;
 
 export const CourseDetailScreen = () => {
   const [selected, setSelected] = useState<CLOType[]>([]);
+  const [clos, setClos] = useState<CLOType[]>();
+  const [plos, setPlos] = useState<PLOType[]>();
   const [modalShown, setModalShown] = useState(false);
   const [updates, setUpdates] = useState(0);
   const route = useRoute<any>();
   const navigation = useNavigation();
   const course: CourseType = route.params!.course;
+
+  const getPloUsage = (id: string) => {
+    if (!clos || !plos) return -1;
+    const plo = plos.find(p => p.id === id);
+    if (!plo) return -1;
+    let weight = 0;
+    clos.forEach(c => {
+      const map = c.maps!.find(map => map.plo!.id === id);
+      if (map) weight += map.weight;
+    });
+    return weight;
+  };
+
   useMemo(() => {
-    criteria = new ManyCriteria<CLOType>();
-    criteria.addRelation('plos');
-    criteria.addCondition('course', course.id);
     navigation.setOptions({headerTitle: course.title + ' CLOs'});
   }, []);
+
+  useEffect(() => {
+    const criteria = new ManyCriteria<CLOType>();
+    criteria.addRelation('maps');
+    criteria.addCondition('course', course.id);
+    cloService
+      .get(criteria)
+      .then(r => setClos(r.data))
+      .catch(e => uiService.toastError('Could not fetch CLOs!'));
+    ploService
+      .get()
+      .then(r => setPlos(r.data))
+      .catch(e => uiService.toastError('Could not fetch PLOs!'));
+  }, [updates]);
+
   return (
-    <>
+    <ScrollView>
       <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
         <IconButton
           icon="delete"
@@ -33,7 +71,17 @@ export const CourseDetailScreen = () => {
           disabled={selected.length < 1}
           color="red"
           onPress={() => {
-            setModalShown(true);
+            Promise.all(selected.map(s => cloService.delete(s.id)))
+              .then(() => {
+                uiService.toastSuccess('Records deleted!');
+              })
+              .catch(e => {
+                uiService.toastSuccess('Failed to delete some records!');
+              })
+              .finally(() => {
+                setUpdates(updates + 1);
+                setSelected([]);
+              });
           }}
         />
         <Button
@@ -47,17 +95,17 @@ export const CourseDetailScreen = () => {
         </Button>
       </View>
       <Card style={{margin: 8}}>
-        <FetchingDataTable
-          key={updates}
-          fetchMethod={c => cloService.get(c)}
-          criteria={criteria}
+        <DataTable
+          data={clos || []}
           columns={[
             {title: 'Title', property: 'title', weight: 0.4},
             {
               title: 'PLOs',
               property: ({item}) => {
                 const plos =
-                  item.plos?.map(p => p.title).join(', ') || 'No PLOs set';
+                  item.maps
+                    ?.map((m, i) => `(${m.plo?.title} - ${m.weight}%)`)
+                    .join(', ') || 'No PLOs set';
                 return <Text>{plos}</Text>;
               },
               weight: 1,
@@ -67,15 +115,46 @@ export const CourseDetailScreen = () => {
           checkProperty="id"
         />
       </Card>
+      <Caption style={{margin: 16}}>Weights Assigned</Caption>
+      <Card style={{margin: 8, padding: 16}}>
+        {plos ? (
+          plos.map(p => {
+            const usage = getPloUsage(p.id);
+            return (
+              <React.Fragment key={p.id}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                  }}>
+                  <Text>{p.title}</Text>
+                  <Caption style={{marginLeft: 'auto'}}>{usage}%</Caption>
+                </View>
+                <ProgressBar
+                  progress={usage / 100}
+                  color={usage === 100 ? colors.green : undefined}
+                />
+                <Divider style={{marginVertical: 8}} />
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <ActivityIndicator />
+        )}
+      </Card>
       <Modal visible={modalShown} onDismiss={() => setModalShown(false)}>
-        <AddCloView
-          onAdd={() => {
-            setUpdates(updates + 1);
-            setModalShown(false);
-          }}
-          courseId={course.id}
-        />
+        {plos ? (
+          <AddCloView
+            onAdd={() => {
+              setUpdates(updates + 1);
+              setModalShown(false);
+            }}
+            plos={plos.map(p => ({...p, weight: getPloUsage(p.id)}))}
+            courseId={course.id}
+          />
+        ) : (
+          <ActivityIndicator />
+        )}
       </Modal>
-    </>
+    </ScrollView>
   );
 };
