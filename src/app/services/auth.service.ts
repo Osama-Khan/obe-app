f83ebc1ffb1f08de94168361088767c518af2409
Osama Khan <Osama6756@gmail.com>
2021-11-15 18:22:58 +0500

    Improved login, added loginWithToken and logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,41 @@
-import axios from 'axios';
+import store from '@app/store';
+import {userActions} from '@app/store/actions';
+import {UserType} from '@app/types';
+import axios, {AxiosResponse} from 'axios';
 import ApiService from './api.service';
+import storageService from './storage.service';
 
+type AuthResponseType = AxiosResponse<UserType & {token: string}>;
 class AuthService extends ApiService {
   endpoint = [this.domain, 'auth'].join('/');
 
-  login(data: {username: string; password: string; remember?: boolean}) {
+  async login(data: {username: string; password: string; remember?: boolean}) {
     const {remember, ...user} = data;
+    const res: AuthResponseType = await axios.post(
+      this.endpoint + '/login',
+      user,
+    );
     if (remember) {
-      //TODO: Store token
+      storageService.setToken(res.data.token);
     }
-    return axios.post(this.endpoint + '/login', user);
+    return res;
+  }
+
+  async loginWithToken() {
+    const token = await storageService.getToken();
+    if (token) {
+      const res: AuthResponseType = await axios.post(this.endpoint + '/token', {
+        token,
+      });
+      res.data.token = token;
+      return res;
+    }
+    return false;
+  }
+
+  logout() {
+    store.dispatch(userActions.clearUser());
+    storageService.clearToken();
   }
 }
 
