057bd799bee027dea5302c3db3fac0659f345436
Osama Khan <Osama6756@gmail.com>
2021-11-15 19:35:46 +0500

    Added login retention

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React, {useMemo, useState} from 'react';
 import {Provider as ReduxProvider} from 'react-redux';
-import {StatusBar} from 'react-native';
-import {Provider} from 'react-native-paper';
+import {StatusBar, View} from 'react-native';
+import {ActivityIndicator, Provider} from 'react-native-paper';
 import {routes} from './app.routes';
 import store from '@app/store';
 import {NavTheme, AppTheme, colors} from '@app/styles';
 import Toast from 'react-native-toast-message';
 import uiService from '@app/services/ui.service';
 import {initializeInterceptors} from '@app/interceptors';
+import authService from '@app/services/auth.service';
+import {userActions} from '@app/store/actions';
+import storageService from '@app/services/storage.service';
 
 const {Navigator, Screen} = createStackNavigator();
 initializeInterceptors();
 
 const App = () => {
   const [stack, setStack] = useState(routes.auth);
+  const [restoring, setRestoring] = useState(true);
   useMemo(() => {
     store.subscribe(() => {
       const user = store.getState().user;
@@ -42,8 +46,33 @@ const App = () => {
       }
       uiService.toastError('Error: Could not find navigation for user type!');
     });
+    authService
+      .loginWithToken()
+      .catch(e => {
+        if (e.response) {
+          uiService.toastError('Login expired!');
+          storageService.clearToken();
+          return;
+        }
+        uiService.toastError('Could not connect to server!');
+      })
+      .finally(() => {
+        setRestoring(false);
+      });
   }, []);
-  return (
+  return restoring ? (
+    <View>
+      <StatusBar hidden />
+      <View
+        style={{
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ActivityIndicator size="large" />
+      </View>
+    </View>
+  ) : (
     <>
       <ReduxProvider store={store}>
         <Provider theme={AppTheme}>
