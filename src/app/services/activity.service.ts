516554b620edd1433ef06188aaedc41507e12ebe
Osama Khan <Osama6756@gmail.com>
2021-11-23 23:12:14 +0500

    Added ActivityService

---
+++ src/app/services/activity.service.ts
@@ -0,0 +1,16 @@
+import {ActivityType} from '@app/types';
+import axios from 'axios';
+import CrudService from './crud.service';
+
+class ActivityService extends CrudService<ActivityType> {
+  constructor() {
+    super('activity');
+  }
+
+  /** Gets all activity types */
+  getTypes() {
+    return axios.post(this.endpoint + '/types');
+  }
+}
+
+export default new ActivityService();
