8c21a6a44daf8a7b5a5130c147e8fe3f40ab1b08
Osama Khan <Osama6756@gmail.com>
2021-12-03 09:22:57 +0500

    Added number property to PLOType

--- src/app/types/plo.type.ts
+++ src/app/types/plo.type.ts
@@ -3,6 +3,7 @@ import DbEntity from './db-entity.type';
 import ObjectiveMapType from './objective-map.type';
 
 type PLOType = DbEntity & {
+  number: number;
   title: string;
   description: string;
   maps?: ObjectiveMapType[];
