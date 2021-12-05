25581c8df798e9e3e90e2ceb145efd529c4c2dbf
Osama Khan <Osama6756@gmail.com>
2021-12-05 13:52:51 +0500

    Added ActivityCard view

---
+++ src/app/screens/teacher/allocation/activities/activity-card.tsx
@@ -0,0 +1,53 @@
+import {colors} from '@app/styles';
+import {ActivityType} from '@app/types';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {Caption, Card, List, Text, Title} from 'react-native-paper';
+
+export default function ActivityCard({activity}: {activity: ActivityType}) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Card
+      style={{
+        margin: 16,
+        marginVertical: 8,
+        overflow: 'hidden',
+      }}>
+      <View style={{padding: 16}}>
+        <Caption
+          style={{
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            color: colors.primary,
+          }}>
+          {activity.type!.name}
+        </Caption>
+        <Title>{activity.title}</Title>
+        <Caption
+          onPress={() => setExpanded(!expanded)}
+          numberOfLines={expanded ? undefined : 1}>
+          {activity.description}
+        </Caption>
+      </View>
+      <List.Accordion title={'CLOs'} style={{backgroundColor: 'white'}}>
+        {activity.maps
+          .map(m => ({...m.clo, weight: m.weight}))
+          .map(c => (
+            <List.Item
+              title={c.title}
+              description={c.description}
+              right={() => (
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    textAlignVertical: 'center',
+                  }}>
+                  {c.weight}%
+                </Text>
+              )}
+            />
+          ))}
+      </List.Accordion>
+    </Card>
+  );
+}
