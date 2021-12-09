811e24ed160053ac998313407ed191b5769d9328
Osama Khan <Osama6756@gmail.com>
2021-12-09 22:11:34 +0500

    Added logout button

--- src/app/screens/student/home.tsx
+++ src/app/screens/student/home.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {Title} from 'react-native-paper';
+import {FAB, Title} from 'react-native-paper';
 import {useSelector} from 'react-redux';
 import {AppStateType} from '@app/store/state';
+import {colors} from '@app/styles';
+import authService from '@app/services/auth.service';
 
 export const Home = () => {
   const user = useSelector((state: AppStateType) => state.user.userData);
   return (
     <>
-      <Title style={{margin: 16}}>Hello {user!.username}</Title>
+      <Title style={{margin: 16}}>Hello {user?.username}</Title>
+      <FAB
+        style={{
+          position: 'absolute',
+          right: 8,
+          bottom: 8,
+          backgroundColor: colors.red,
+        }}
+        icon="logout"
+        onPress={authService.logout}
+      />
     </>
   );
 };
