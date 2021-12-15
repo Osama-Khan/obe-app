55c70be324e61e56ad119f607bf671512973a298
osama-khan <osama6756@gmail.com>
2021-12-15 21:33:13 +0500

    Added locked and expanded properties

--- src/app/components/modal/bottom-up-modal.tsx
+++ src/app/components/modal/bottom-up-modal.tsx
@@ -1,53 +1,62 @@
 import {AppTheme} from '@app/styles';
 import React from 'react';
-import {
-  ScrollView,
-  PanResponder,
-  PanResponderInstance,
-  Dimensions,
-  ModalProps,
-} from 'react-native';
+import {ScrollView, PanResponder, Dimensions, ModalProps} from 'react-native';
 import Reanimated, {EasingNode} from 'react-native-reanimated';
 import Icon from '../icon';
 import ModalBase from './modal-base';
 
-export default class BottomUpModal extends React.Component<ModalProps, any> {
-  height = new Reanimated.Value(this.collapsedHeight);
-  heightValue = this.collapsedHeight;
-  modalHeight = this.collapsedHeight;
-  _panResponder: PanResponderInstance = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderGrant: () => {
-      const modalExpanded = this.modalHeight > this.heightCenter;
-      this.modalHeight = modalExpanded
-        ? this.expandedHeight
-        : this.collapsedHeight;
-    },
-    onPanResponderMove: (_, g) => {
-      const height = this.modalHeight - g.dy;
-      this.heightValue = height;
-      this.height.setValue(height);
-    },
-    onPanResponderRelease: (_, g) => {
-      // Should dismiss if height too small
-      if (this.heightValue < this.windowHeight * 0.2) {
-        this.props.onDismiss();
-        this.height.setValue(this.collapsedHeight);
-        return;
-      }
+export type BottomUpModalProps = ModalProps & {
+  locked?: boolean;
+  expanded?: boolean;
+};
+export default class BottomUpModal extends React.Component<
+  BottomUpModalProps,
+  any
+> {
+  height = new Reanimated.Value(
+    this.props.expanded ? this.expandedHeight : this.collapsedHeight,
+  );
+  heightValue = this.props.expanded
+    ? this.expandedHeight
+    : this.collapsedHeight;
+  modalHeight = this.props.expanded
+    ? this.expandedHeight
+    : this.collapsedHeight;
+  _panResponder = this.props.locked
+    ? undefined
+    : PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+          const modalExpanded = this.modalHeight > this.heightCenter;
+          this.modalHeight = modalExpanded
+            ? this.expandedHeight
+            : this.collapsedHeight;
+        },
+        onPanResponderMove: (_, g) => {
+          const height = this.modalHeight - g.dy;
+          this.heightValue = height;
+          this.height.setValue(height);
+        },
+        onPanResponderRelease: (_, g) => {
+          // Should dismiss if height too small
+          if (this.heightValue < this.windowHeight * 0.2) {
+            this.props.onDismiss && this.props.onDismiss();
+            this.height.setValue(this.collapsedHeight);
+            return;
+          }
 
-      if (g.dy < 0) {
-        this.modalHeight = this.expandedHeight;
-      } else {
-        this.modalHeight = this.collapsedHeight;
-      }
-      Reanimated.timing(this.height, {
-        toValue: this.modalHeight,
-        duration: 200,
-        easing: EasingNode.out(EasingNode.ease),
-      }).start();
-    },
-  });
+          if (g.dy < 0) {
+            this.modalHeight = this.expandedHeight;
+          } else {
+            this.modalHeight = this.collapsedHeight;
+          }
+          Reanimated.timing(this.height, {
+            toValue: this.modalHeight,
+            duration: 200,
+            easing: EasingNode.out(EasingNode.ease),
+          }).start();
+        },
+      });
 
   render() {
     const {children, ...other} = this.props;
@@ -69,13 +78,17 @@ export default class BottomUpModal extends React.Component<ModalProps, any> {
           }}>
           <Reanimated.View
             style={[modalStyle, {height: this.height}]}
-            {...this._panResponder.panHandlers}>
-            <Icon
-              name="drag-horizontal-variant"
-              color="#888"
-              size={24}
-              style={{alignSelf: 'center'}}
-            />
+            {...(this._panResponder ? this._panResponder.panHandlers : {})}>
+            {this.props.locked ? (
+              <></>
+            ) : (
+              <Icon
+                name="drag-horizontal-variant"
+                color="#888"
+                size={24}
+                style={{alignSelf: 'center'}}
+              />
+            )}
             <ScrollView>{children}</ScrollView>
           </Reanimated.View>
         </ModalBase>
