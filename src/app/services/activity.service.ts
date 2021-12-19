daeadc9b2c453847be522974637dc11de15b851b
Osama Khan <Osama6756@gmail.com>
2021-12-19 19:07:21 +0500

    Added endpoint to find cumulative weights of CLOs

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -11,6 +11,13 @@ class ActivityService extends CrudService<ActivityType> {
   getTypes() {
     return axios.post(this.endpoint + '-type');
   }
+
+  /** Gets CLOs with their cumulative weights in given section
+   * @param id ID of the section
+   */
+  getCloWeightsInSection(id: string) {
+    return axios.get(`${this.endpoint}/maps/${id}`);
+  }
 }
 
 export default new ActivityService();
