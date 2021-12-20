ce68a88a6af0afd788e8515758fe59ff7d8ac935
Osama Khan <Osama6756@gmail.com>
2021-12-20 08:23:23 +0500

    Removed extra state variable CLOs

--- src/app/screens/teacher/allocation/activities/index.tsx
+++ src/app/screens/teacher/allocation/activities/index.tsx
@@ -1,9 +1,8 @@
 import {ManyCriteria} from '@app/models/criteria';
 import {addActivityRoute} from '@app/routes/teacher.routes';
 import activityService from '@app/services/activity.service';
-import cloService from '@app/services/clo.service';
 import uiService from '@app/services/ui.service';
-import {ActivityType, AllocationType, CLOType, ProgramType} from '@app/types';
+import {ActivityType, AllocationType, ProgramType} from '@app/types';
 import {useNavigation, useRoute} from '@react-navigation/core';
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
@@ -12,32 +11,13 @@ import ActivityCard from './activity-card';
 
 export const ActivitiesScreen = () => {
   const [activities, setActivities] = useState<ActivityType[]>();
-  const [clos, setClos] = useState<CLOType[]>();
   const [updates, setUpdates] = useState(0);
   const navigation = useNavigation<any>();
   const route = useRoute<any>();
   const allocation: AllocationType & {program: ProgramType} =
     route.params!.allocation;
 
-  const getCloUsage = (id: string) => {
-    if (!clos || !activities) return -1;
-    const clo = clos.find(c => c.id === id);
-    if (!clo) return -1;
-    let weight = 0;
-    activities.forEach(a => {
-      const map = a.maps!.find(map => map.clo!.id === id);
-      if (map) weight += map.weight;
-    });
-    return weight;
-  };
-
   useEffect(() => {
-    const cloCrit = new ManyCriteria<CLOType>();
-    cloCrit.addCondition('course', allocation.course!.id);
-    cloService
-      .get(cloCrit)
-      .then(r => setClos(r.data))
-      .catch(e => uiService.toastError('Could not fetch CLOs!'));
     const activityCrit = new ManyCriteria<ActivityType>();
     activityCrit.addRelation('maps');
     activityCrit.addRelation('type');
@@ -80,10 +60,6 @@ export const ActivitiesScreen = () => {
               setUpdates(updates + 1);
             },
             allocation,
-            clos: clos?.map(c => ({
-              ...c,
-              weight: getCloUsage(c.id),
-            })),
           });
         }}
       />
