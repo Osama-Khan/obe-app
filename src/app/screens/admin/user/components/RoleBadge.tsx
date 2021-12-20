34d371a90470edd940ad730b599febbf9e8ad11f
Osama Khan <Osama6756@gmail.com>
2021-12-21 03:02:30 +0500

    Added RoleBadge component

---
+++ src/app/screens/admin/user/components/RoleBadge.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {colors} from '@app/styles';
+import {StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+export default function RoleBadge({roleName}: {roleName: string}) {
+  return (
+    <View style={[styles.roleView, getRoleStyle(roleName)]}>
+      <Text style={[{fontWeight: 'bold'}, getRoleTextStyle(roleName)]}>
+        {roleName.toUpperCase()}
+      </Text>
+    </View>
+  );
+}
+
+const getRoleStyle = (roleName: string) => {
+  switch (roleName) {
+    case 'admin':
+      return styles.roleAdminView;
+    case 'hod':
+      return styles.roleHodView;
+    case 'teacher':
+      return styles.roleTeacherView;
+    default:
+      return styles.roleStudentView;
+  }
+};
+
+const getRoleTextStyle = (roleName: string) => {
+  switch (roleName) {
+    case 'admin':
+      return styles.roleAdminText;
+    case 'hod':
+      return styles.roleHodText;
+    case 'teacher':
+      return styles.roleTeacherText;
+    default:
+      return styles.roleStudentText;
+  }
+};
+
+const styles = StyleSheet.create({
+  roleView: {
+    padding: 4,
+    borderRadius: 4,
+  },
+  roleAdminView: {
+    backgroundColor: colors.primarySubtle,
+  },
+  roleAdminText: {
+    color: colors.primaryDark,
+  },
+  roleHodView: {
+    backgroundColor: colors.greenSubtle,
+  },
+  roleHodText: {
+    color: colors.greenDark,
+  },
+  roleTeacherView: {
+    backgroundColor: colors.yellowSubtle,
+  },
+  roleTeacherText: {
+    color: colors.yellowDark,
+  },
+  roleStudentView: {
+    backgroundColor: colors.slateSubtle,
+  },
+  roleStudentText: {
+    color: colors.slateDark,
+  },
+});
