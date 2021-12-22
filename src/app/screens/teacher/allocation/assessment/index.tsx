c7300f11c92585dbf46144bf6ad3b8397e5c79c9
Osama Khan <Osama6756@gmail.com>
2021-12-23 04:13:59 +0500

    Added BottomNav to allow switching between MapTable and PieChart

--- src/app/screens/teacher/allocation/assessment/index.tsx
+++ src/app/screens/teacher/allocation/assessment/index.tsx
@@ -1,38 +1,68 @@
 import {useRoute} from '@react-navigation/core';
-import React from 'react';
-import {ActivityIndicator, useWindowDimensions} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
-import {Caption, List} from 'react-native-paper';
+import React, {useState} from 'react';
+import {ActivityIndicator, useWindowDimensions, View} from 'react-native';
+import {BottomNavigation, Caption, List} from 'react-native-paper';
 import useAssessments from '@app/hooks/useAssessments';
-import WeightsCard from './weights-card';
+import WeightsGraph from './weights-graph';
+import WeightsTable from './weights-table';
 
 export const AssessmentScreen = () => {
   const dimens = useWindowDimensions();
   const route = useRoute<any>();
 
   const allocation = route.params.allocation;
-
   const {assessments, types, clos} = useAssessments(allocation.id);
 
+  const TableRoute = () => (
+    <WeightsTable
+      style={{marginHorizontal: 16, overflow: 'hidden'}}
+      assessments={assessments!}
+      clos={clos!}
+      types={types!}
+    />
+  );
+
+  const GraphRoute = () => (
+    <WeightsGraph
+      style={{marginHorizontal: 16, overflow: 'hidden'}}
+      assessments={assessments!}
+      clos={clos!}
+      types={types!}
+      chartWidth={dimens.width - 32}
+    />
+  );
+
+  const [index, setIndex] = useState(0);
+
+  const [routes] = React.useState([
+    {key: 'table', title: 'Table', icon: 'table'},
+    {key: 'graph', title: 'Graph', icon: 'chart-pie'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    table: TableRoute,
+    graph: GraphRoute,
+  });
+
   return (
-    <ScrollView>
-      <List.Section title="CLO Distribution">
-        {assessments ? (
-          assessments.length > 0 ? (
-            <WeightsCard
-              style={{marginHorizontal: 16, overflow: 'hidden'}}
-              assessments={assessments}
-              clos={clos!}
-              types={types!}
-              chartWidth={dimens.width - 32}
+    <View style={{flexGrow: 1}}>
+      {assessments ? (
+        assessments.length > 0 ? (
+          <>
+            <List.Section title="CLO Distribution" />
+            <BottomNavigation
+              navigationState={{index, routes}}
+              onIndexChange={setIndex}
+              renderScene={renderScene}
+              shifting={true}
             />
-          ) : (
-            <Caption style={{alignSelf: 'center'}}>No Assessment Data</Caption>
-          )
+          </>
         ) : (
-          <ActivityIndicator />
-        )}
-      </List.Section>
-    </ScrollView>
+          <Caption style={{alignSelf: 'center'}}>No Assessment Data</Caption>
+        )
+      ) : (
+        <ActivityIndicator />
+      )}
+    </View>
   );
 };
