c191554706032c341943213247e264a3b36e3525
Osama Khan <Osama6756@gmail.com>
2021-12-29 07:46:28 +0500

    Added results table in Student home

--- src/app/screens/student/home.tsx
+++ src/app/screens/student/home.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import {FAB, Title} from 'react-native-paper';
+import {Caption, Card, FAB, Text, Title} from 'react-native-paper';
 import {useSelector} from 'react-redux';
 import {AppStateType} from '@app/store/state';
 import {colors} from '@app/styles';
 import authService from '@app/services/auth.service';
+import {ScrollView, View} from 'react-native';
+import {FetchingDataTable} from '@app/components/data-table';
+import userService from '@app/services/user.service';
 
 export const Home = () => {
   const user = useSelector((state: AppStateType) => state.user.userData);
   return (
     <>
-      <Title style={{margin: 16}}>Hello {user?.username}</Title>
+      <ScrollView>
+        <View style={{margin: 16}}>
+          <Title>Hello {user?.username}</Title>
+          <Caption>Your result is as follows</Caption>
+          <Card>
+            <FetchingDataTable
+              fetchMethod={c => userService.getResults(user!.id)}
+              columns={[
+                {
+                  title: 'PLO',
+                  selector: ({item}: any) => <Text>PLO {item.plo.number}</Text>,
+                },
+                {
+                  title: 'Evaluated',
+                  selector: ({item}: any) => (
+                    <Text>{item.evaluated.toFixed(2)}%</Text>
+                  ),
+                },
+                {
+                  title: 'Achieved',
+                  selector: ({item}: any) => (
+                    <Text>{item.achieved.toFixed(2)}%</Text>
+                  ),
+                },
+              ]}
+            />
+          </Card>
+        </View>
+      </ScrollView>
       <FAB
         style={{
           position: 'absolute',
