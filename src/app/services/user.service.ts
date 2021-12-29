946f8890c826368edae40858b3635b42b77f2b9a
Osama Khan <Osama6756@gmail.com>
2021-12-29 07:45:46 +0500

    Added getResults method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import UserType from '@app/types/user.type';
+import axios from 'axios';
 import CrudService from './crud.service';
 
 class UserService extends CrudService<UserType> {
   constructor() {
     super('user');
+  }
+
+  /**
+   * Returns result of the student
+   * @param id ID of the student
+   * @returns Result represented by percentage obtained in PLOs
+   */
+  getResults(id: string) {
+    return axios.get(`${this.endpoint}/${id}/result`);
   }
 }
 
