f9430c4d01b88e86c6d7d904b6138dbd9c58f985
Osama Khan <Osama6756@gmail.com>
2022-01-01 10:57:13 +0500

    Added studentResultsRoute

---
+++ src/app/routes/shared.routes.ts
@@ -0,0 +1,8 @@
+import {StudentResultsScreen} from '@app/screens/shared';
+import {RouteType} from '@app/types/route.type';
+
+export const studentResultsRoute: RouteType = {
+  id: 'student-results',
+  name: 'Student Results',
+  component: StudentResultsScreen,
+};
