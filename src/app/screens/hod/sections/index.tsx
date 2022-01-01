f0b1bdbd180882b8aac480bbbf4fb831c585fcbd
Osama Khan <Osama6756@gmail.com>
2022-01-01 12:43:10 +0500

    Added FAB to add section

--- src/app/screens/hod/sections/index.tsx
+++ src/app/screens/hod/sections/index.tsx
@@ -1,34 +1,63 @@
 import {FetchingDataTable} from '@app/components/data-table';
 import {ManyCriteria} from '@app/models/criteria';
 import sectionService from '@app/services/section.service';
+import uiService from '@app/services/ui.service';
 import {SectionType} from '@app/types';
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView} from 'react-native';
-import {Card, Text} from 'react-native-paper';
+import {Card, FAB, Text} from 'react-native-paper';
+import AddSectionModal from './add-section.modal';
 
 export default function SectionsScreen() {
+  const [updates, setUpdates] = useState(0);
+  const [shown, setShown] = useState(false);
+  const hideModal = () => setShown(false);
   return (
-    <ScrollView>
-      <Card style={{margin: 16, marginBottom: 96}}>
-        <FetchingDataTable
-          criteria={new ManyCriteria<SectionType>({relations: ['program']})}
-          fetchMethod={c => sectionService.get(c)}
-          columns={[
-            {
-              title: 'Program',
-              selector: ({item}) => <Text>{item.program!.title}</Text>,
-            },
-            {
-              title: 'Semester',
-              selector: 'semester',
-            },
-            {
-              title: 'Name',
-              selector: 'name',
-            },
-          ]}
-        />
-      </Card>
-    </ScrollView>
+    <>
+      <ScrollView>
+        <Card style={{margin: 16, marginBottom: 96}}>
+          <FetchingDataTable
+            key={updates}
+            criteria={new ManyCriteria<SectionType>({relations: ['program']})}
+            fetchMethod={c => sectionService.get(c)}
+            columns={[
+              {
+                title: 'Program',
+                selector: ({item}) => <Text>{item.program!.title}</Text>,
+              },
+              {
+                title: 'Semester',
+                selector: 'semester',
+              },
+              {
+                title: 'Name',
+                selector: 'name',
+              },
+            ]}
+          />
+        </Card>
+      </ScrollView>
+      <FAB
+        icon="plus"
+        style={{position: 'absolute', bottom: 16, right: 16}}
+        onPress={() => {
+          setShown(true);
+        }}
+      />
+      <AddSectionModal
+        onAdd={data => {
+          hideModal();
+          sectionService
+            .insert(data)
+            .then(() => {
+              setUpdates(updates + 1);
+              uiService.toastSuccess('Successfully added Section!');
+            })
+            .catch(() => uiService.toastError('Could not add Section!'));
+        }}
+        onDismiss={hideModal}
+        visible={shown}
+      />
+    </>
   );
 }
