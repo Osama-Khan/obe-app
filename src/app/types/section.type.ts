542c46b91fc303b6f42c75ce7ca90bf97180197e
Osama Khan <Osama6756@gmail.com>
2022-01-01 10:56:50 +0500

    Removed extra import

--- src/app/types/section.type.ts
+++ src/app/types/section.type.ts
@@ -1,7 +1,6 @@
 import UserType from './user.type';
 import AllocationType from './allocation.type';
 import ProgramType from './program.type';
-import CourseType from './course.type';
 import DbEntity from './db-entity.type';
 
 type SectionType = DbEntity & {
