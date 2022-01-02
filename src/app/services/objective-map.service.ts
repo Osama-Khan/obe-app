767e06d935fa2eface7f68f4ac801a24cd87ebac
Osama Khan <Osama6756@gmail.com>
2022-01-02 15:24:59 +0500

    Added getCourseMaps method

--- src/app/services/objective-map.service.ts
+++ src/app/services/objective-map.service.ts
@@ -1,9 +1,18 @@
 import {ObjectiveMapType} from '@app/types';
+import axios, {AxiosResponse} from 'axios';
 import CrudService from './crud.service';
 
 class ObjectiveMapService extends CrudService<ObjectiveMapType> {
   constructor() {
     super('objective-map');
+  }
+
+  /** Gets the maps for given program and course */
+  getCourseMaps(
+    pid: string,
+    cid: string,
+  ): Promise<AxiosResponse<ObjectiveMapType[]>> {
+    return axios.get(`${this.endpoint}/program/${pid}/course/${cid}`);
   }
 }
 
