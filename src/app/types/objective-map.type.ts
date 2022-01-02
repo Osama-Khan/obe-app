f1fc7e193f8c4ed8b073b1bdcf3a2e3856488adf
Osama Khan <Osama6756@gmail.com>
2022-01-02 15:37:50 +0500

    Added program in ObjectiveMapType

--- src/app/types/objective-map.type.ts
+++ src/app/types/objective-map.type.ts
@@ -1,11 +1,13 @@
 import PLOType from './plo.type';
 import CLOType from './clo.type';
 import DbEntity from './db-entity.type';
+import ProgramType from './program.type';
 
 type ObjectiveMapType = DbEntity & {
   clo?: Partial<CLOType>;
   plo?: Partial<PLOType>;
   weight: number;
+  program?: Partial<ProgramType>;
 };
 
 export default ObjectiveMapType;
