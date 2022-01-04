1f55551a4327f521a79b28db7e8ce076cbda5cb7
Osama Khan <Osama6756@gmail.com>
2022-01-04 10:28:06 +0500

    Added getResultDetail endpoint

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,6 +15,16 @@ class UserService extends CrudService<UserType> {
   getResults(id: string) {
     return axios.get(`${this.endpoint}/${id}/result`);
   }
+
+  /**
+   * Returns detailed result of the student
+   * @param id ID of the student
+   * @param ploId ID of the plo
+   * @returns Result object containing detailed evaluations
+   */
+  getResultDetail(id: string, ploId: string) {
+    return axios.get(`${this.endpoint}/${id}/result/plo/${ploId}`);
+  }
 }
 
 export default new UserService();
