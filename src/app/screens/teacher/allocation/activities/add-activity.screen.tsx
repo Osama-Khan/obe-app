f222d77b17a5bdc3a80f90156c7a51642e63d2e1
Osama Khan <Osama6756@gmail.com>
2022-01-16 21:55:11 +0500

    Improved Activity addition experience Added check to prevent adding activity without approved assessment Added appropriate messages for invalid assessments

--- src/app/screens/teacher/allocation/activities/add-activity.screen.tsx
+++ src/app/screens/teacher/allocation/activities/add-activity.screen.tsx
@@ -12,7 +12,7 @@ import {
 } from '@app/types';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {
   ActivityIndicator,
   Button,
@@ -84,167 +84,177 @@ export default function AddActivityScreen() {
   const count = counts ? counts.find(c => c.id === type?.id!)?.count || 0 : -1;
   const marksError = parseInt(marks) <= 0;
 
-  return cloWeights ? (
-    <>
-      <ScrollView>
-        <Card style={{margin: 8, padding: 8}}>
-          <Title>
-            {type
-              ? count !== -1
-                ? `${type.name} ${count + 1}`
-                : 'Loading...'
-              : 'Select Type'}
-          </Title>
-          <>
-            <TextInput
-              mode="outlined"
-              label="Total Marks"
-              onChangeText={txt => {
-                if (new RegExp('^[0-9]*$').test(txt)) {
-                  setMarks(txt);
-                }
-              }}
-              error={marksError}
-              maxLength={3}
-              value={marks}
-              keyboardType="decimal-pad"
-            />
-            {marksError && (
-              <Caption style={{color: colors.red}}>
-                Marks must be greater than 0!
-              </Caption>
+  return cloWeights && assessments ? (
+    assessments.length > 0 && assessments[0].isApproved ? (
+      <>
+        <ScrollView>
+          <Card style={{margin: 8, padding: 8}}>
+            <Title>
+              {type
+                ? count !== -1
+                  ? `${type.name} ${count + 1}`
+                  : 'Loading...'
+                : 'Select Type'}
+            </Title>
+            <>
+              <TextInput
+                mode="outlined"
+                label="Total Marks"
+                onChangeText={txt => {
+                  if (new RegExp('^[0-9]*$').test(txt)) {
+                    setMarks(txt);
+                  }
+                }}
+                error={marksError}
+                maxLength={3}
+                value={marks}
+                keyboardType="decimal-pad"
+              />
+              {marksError && (
+                <Caption style={{color: colors.red}}>
+                  Marks must be greater than 0!
+                </Caption>
+              )}
+            </>
+            <Caption style={{marginTop: 8}}>Choose Type</Caption>
+            {types ? (
+              <ListSelect
+                options={[
+                  {name: 'Select a Type...', value: '', disabled: true},
+                  ...types.map(c => ({name: c.name, value: c.id})),
+                ]}
+                onSelect={o => setType(types.find(c => c.id === o.value)!)}
+              />
+            ) : (
+              <ActivityIndicator />
             )}
-          </>
-          <Caption style={{marginTop: 8}}>Choose Type</Caption>
-          {types ? (
-            <ListSelect
-              options={[
-                {name: 'Select a Type...', value: '', disabled: true},
-                ...types.map(c => ({name: c.name, value: c.id})),
-              ]}
-              onSelect={o => setType(types.find(c => c.id === o.value)!)}
-            />
-          ) : (
-            <ActivityIndicator />
-          )}
-          {clos ? (
-            clos.length > 0 ? (
+            {clos ? (
+              clos.length > 0 ? (
+                <>
+                  <Caption style={{marginTop: 8}}>Add CLOs</Caption>
+                  {clos.map((c, i) => {
+                    const weight = getCloUsage(c.id);
+                    return (
+                      <WeightPicker
+                        title={`CLO ${c.number}`}
+                        description={`${weight}% Assigned`}
+                        onChange={(weight, err) => {
+                          added![i] = {...c, weight, err};
+                          setAdded([...added!]);
+                        }}
+                        onRemove={() => {
+                          added![i] = undefined;
+                          setAdded([...added!]);
+                        }}
+                        usedWeight={weight}
+                      />
+                    );
+                  })}
+                </>
+              ) : (
+                <Caption style={{color: colors.red}}>
+                  No CLOs Available for this course!
+                </Caption>
+              )
+            ) : type ? (
+              <ActivityIndicator />
+            ) : (
+              <></>
+            )}
+            {added && added.some(a => a && !a.err) ? (
               <>
-                <Caption style={{marginTop: 8}}>Add CLOs</Caption>
-                {clos.map((c, i) => {
-                  const weight = getCloUsage(c.id);
-                  return (
-                    <WeightPicker
-                      title={`CLO ${c.number}`}
-                      description={`${weight}% Assigned`}
-                      onChange={(weight, err) => {
-                        added![i] = {...c, weight, err};
-                        setAdded([...added!]);
-                      }}
-                      onRemove={() => {
-                        added![i] = undefined;
-                        setAdded([...added!]);
-                      }}
-                      usedWeight={weight}
-                    />
-                  );
-                })}
+                <Divider />
+
+                <Title>Questions</Title>
+                {questions.map((q, i) => (
+                  <List.Item
+                    title={q.title}
+                    right={() => (
+                      <IconButton
+                        icon="close"
+                        color={colors.red}
+                        style={{marginVertical: 'auto'}}
+                        onPress={() =>
+                          setQuestions(questions.filter((_, ind) => ind !== i))
+                        }
+                      />
+                    )}
+                  />
+                ))}
+                <Button icon="plus" onPress={() => setModal(true)}>
+                  Add Question
+                </Button>
               </>
             ) : (
-              <Caption style={{color: colors.red}}>
-                No CLOs Available for this course!
-              </Caption>
-            )
-          ) : type ? (
-            <ActivityIndicator />
-          ) : (
-            <></>
-          )}
-          {added && added.some(a => a && !a.err) ? (
-            <>
-              <Divider />
-
-              <Title>Questions</Title>
-              {questions.map((q, i) => (
-                <List.Item
-                  title={q.title}
-                  right={() => (
-                    <IconButton
-                      icon="close"
-                      color={colors.red}
-                      style={{marginVertical: 'auto'}}
-                      onPress={() =>
-                        setQuestions(questions.filter((_, ind) => ind !== i))
-                      }
-                    />
-                  )}
-                />
-              ))}
-              <Button icon="plus" onPress={() => setModal(true)}>
-                Add Question
-              </Button>
-            </>
-          ) : (
-            <></>
-          )}
-          <Button
-            style={{marginTop: 16}}
-            icon="check"
-            mode="contained"
-            disabled={
-              !type ||
-              !added ||
-              added.length === 0 ||
-              saving ||
-              !marks ||
-              marks === '0' ||
-              added.some(a => a && a.err)
-            }
-            loading={saving}
-            onPress={() => {
-              setSaving(true);
-              const activity = {
-                title: `${type!.name!} ${count + 1}`,
-                marks,
-                type: {id: type!.id},
-                allocation: {id: allocation.id},
-                maps: added!
-                  .filter(c => c)
-                  .map(c => ({
-                    clo: {id: c!.id},
-                    weight: c!.weight,
-                  })),
-                questions,
-              };
-              activityService
-                .insert(activity)
-                .then(r => {
-                  onAdd(r.data);
-                  uiService.toastSuccess('Added Exam successfully!');
-                  navigation.goBack();
-                })
-                .catch(e => {
-                  uiService.toastError('Failed to add Exam!');
-                })
-                .finally(() => {
-                  setSaving(false);
-                });
-            }}>
-            Add
-          </Button>
-        </Card>
-      </ScrollView>
-      {clos && modal && (
-        <AddQuestionModal
-          visible={modal}
-          onAdd={q => {
-            setQuestions([...questions, q]);
-          }}
-          clos={added?.filter(a => a && !a.err)}
-          onDismiss={() => setModal(false)}
-        />
-      )}
-    </>
+              <></>
+            )}
+            <Button
+              style={{marginTop: 16}}
+              icon="check"
+              mode="contained"
+              disabled={
+                !type ||
+                !added ||
+                added.length === 0 ||
+                saving ||
+                !marks ||
+                marks === '0' ||
+                added.some(a => a && a.err)
+              }
+              loading={saving}
+              onPress={() => {
+                setSaving(true);
+                const activity = {
+                  title: `${type!.name!} ${count + 1}`,
+                  marks,
+                  type: {id: type!.id},
+                  allocation: {id: allocation.id},
+                  maps: added!
+                    .filter(c => c)
+                    .map(c => ({
+                      clo: {id: c!.id},
+                      weight: c!.weight,
+                    })),
+                  questions,
+                };
+                activityService
+                  .insert(activity)
+                  .then(r => {
+                    onAdd(r.data);
+                    uiService.toastSuccess('Added Exam successfully!');
+                    navigation.goBack();
+                  })
+                  .catch(e => {
+                    uiService.toastError('Failed to add Exam!');
+                  })
+                  .finally(() => {
+                    setSaving(false);
+                  });
+              }}>
+              Add
+            </Button>
+          </Card>
+        </ScrollView>
+        {clos && modal && (
+          <AddQuestionModal
+            visible={modal}
+            onAdd={q => {
+              setQuestions([...questions, q]);
+            }}
+            clos={added?.filter(a => a && !a.err)}
+            onDismiss={() => setModal(false)}
+          />
+        )}
+      </>
+    ) : (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Caption>
+          {assessments.length === 0
+            ? 'This course has not been assessed yet!'
+            : 'The assessment for this course has not been approved yet!'}
+        </Caption>
+      </View>
+    )
   ) : (
     <ActivityIndicator style={{flex: 1}} />
   );
