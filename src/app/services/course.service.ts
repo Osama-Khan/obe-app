cd7d32bfea804a1e579df6c47477baf3dd10d339
Osama Khan <Osama6756@gmail.com>
2022-01-19 22:00:52 +0500

    Added course/getWithActions endpoint
    This endpoint adds props to the response indicating any need for actions

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,9 +1,25 @@
 import CourseType from '@app/types/course.type';
+import axios, {AxiosResponse} from 'axios';
 import CrudService from './crud.service';
 
 class CourseService extends CrudService<CourseType> {
   constructor() {
     super('course');
+  }
+
+  /** Gets list of courses with booleans representing if attention is required
+   * @param id ID of program to fetch courses of
+   */
+  getWithActions(id: string): Promise<
+    AxiosResponse<
+      (CourseType & {
+        needsPlos: boolean;
+        needsClos: boolean;
+        needsAssessment: boolean;
+      })[]
+    >
+  > {
+    return axios.get(`${this.endpoint}/with-actions/${id}`);
   }
 }
 
