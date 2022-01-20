2ac8c3ada8afcf9d59ad9846862c9bd1ec342a61
Osama Khan <Osama6756@gmail.com>
2022-01-20 19:32:00 +0500

    Improved CLOList edit Icon

--- src/app/components/CloList/index.tsx
+++ src/app/components/CloList/index.tsx
@@ -23,76 +23,87 @@ export const CLOList = ({clos, onDelete, onEdit, ...props}: P) => (
   <FlatList
     {...props}
     data={clos}
-    renderItem={({item}) => (
-      <Card
-        style={{
-          borderWidth: item.maps![0].weight > 0 ? undefined : 2,
-          borderTopWidth: item.maps![0].weight === 0 ? undefined : 2,
-          borderColor: colors.primary,
-          margin: 16,
-          marginVertical: 8,
-          overflow: 'hidden',
-        }}>
-        <View style={{padding: 8}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Title>CLO {item.number}</Title>
-            {!!onDelete && (
-              <IconButton
-                style={{marginLeft: 'auto'}}
-                color={colors.red}
-                icon="link-off"
-                onPress={() => onDelete(item)}
-              />
-            )}
-            {!!onEdit && (
-              <IconButton
-                style={{marginLeft: 'auto'}}
-                color={colors.primary}
-                icon="pencil"
-                onPress={() => onEdit(item)}
-              />
-            )}
-          </View>
-          {item.maps![0].weight === 0 && (
-            <Badge
+    renderItem={({item}) => {
+      const needsWeights = item.maps![0].weight === 0;
+      return (
+        <Card
+          style={{
+            borderWidth: needsWeights ? 2 : undefined,
+            borderTopWidth: 2,
+            borderColor: colors.primary,
+            margin: 16,
+            marginVertical: 8,
+            overflow: 'hidden',
+          }}>
+          <View style={{padding: 8}}>
+            <View
               style={{
-                backgroundColor: colors.primary,
-                alignSelf: 'flex-start',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                margin: -2,
-                borderTopRightRadius: 0,
-                borderBottomLeftRadius: 0,
-              }}
-              onPress={() =>
-                Alert.alert(
-                  'Needs weights',
-                  'This CLO has been mapped but needs weights from HOD to be effective.',
-                )
-              }>
-              <Icon name="exclamation-thick" />
-              Needs weights
-            </Badge>
-          )}
-          <Caption>
-            {item.title}: {item.description}
-          </Caption>
-        </View>
-        <Divider />
-        <List.Accordion title="Mapped PLOs" style={{backgroundColor: 'white'}}>
-          {item.maps!.map(m => (
-            <List.Item
-              title={m.plo!.title}
-              right={() => (
-                <Text style={{textAlignVertical: 'center', fontWeight: 'bold'}}>
-                  {m.weight}%
-                </Text>
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Title>CLO {item.number}</Title>
+              {!!onDelete && (
+                <IconButton
+                  color={colors.red}
+                  icon="link-off"
+                  onPress={() => onDelete(item)}
+                />
               )}
-            />
-          ))}
-        </List.Accordion>
-      </Card>
-    )}
+              {!!onEdit && (
+                <IconButton
+                  color={colors.primary}
+                  icon={
+                    needsWeights ? 'checkbox-multiple-marked-circle' : 'pencil'
+                  }
+                  onPress={() => onEdit(item)}
+                />
+              )}
+            </View>
+            {needsWeights && (
+              <Badge
+                style={{
+                  backgroundColor: colors.primary,
+                  alignSelf: 'flex-start',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  margin: -2,
+                  borderTopRightRadius: 0,
+                  borderBottomLeftRadius: 0,
+                }}
+                onPress={() =>
+                  Alert.alert(
+                    'Needs weights',
+                    'This CLO has been mapped but needs weights from HOD to be effective.',
+                  )
+                }>
+                <Icon name="exclamation-thick" />
+                Needs weights
+              </Badge>
+            )}
+            <Caption>
+              {item.title}: {item.description}
+            </Caption>
+          </View>
+          <Divider />
+          <List.Accordion
+            title="Mapped PLOs"
+            style={{backgroundColor: 'white'}}>
+            {item.maps!.map(m => (
+              <List.Item
+                title={m.plo!.title}
+                right={() => (
+                  <Text
+                    style={{textAlignVertical: 'center', fontWeight: 'bold'}}>
+                    {m.weight}%
+                  </Text>
+                )}
+              />
+            ))}
+          </List.Accordion>
+        </Card>
+      );
+    }}
   />
 );
