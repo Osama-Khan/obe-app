d64a15b3f83e84330a1489c8e3c27d24e2ebda83
Osama Khan <Osama6756@gmail.com>
2022-01-21 21:16:03 +0500

    Implemented FCAR functionality

--- src/app/screens/teacher/allocation/fcar/fcar.screen.tsx
+++ src/app/screens/teacher/allocation/fcar/fcar.screen.tsx
@@ -1,20 +1,88 @@
-import {AllocationType} from '@app/types';
+import {ManyCriteria} from '@app/models/criteria';
+import activityService from '@app/services/activity.service';
+import cloService from '@app/services/clo.service';
+import uiService from '@app/services/ui.service';
+import {ActivityType, AllocationType, CLOType} from '@app/types';
 import {useRoute} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView} from 'react-native';
-import {Text, Card, Title, Caption} from 'react-native-paper';
+import {
+  Text,
+  Card,
+  Title,
+  Caption,
+  Divider,
+  ActivityIndicator,
+} from 'react-native-paper';
+import FCARTable from './fcar.table';
+
+const useClos = (courseId: string) => {
+  const [clos, setClos] = useState<CLOType[]>();
+  useEffect(() => {
+    const criteria = new ManyCriteria<CLOType>();
+    criteria.addCondition('course', courseId);
+    criteria.addRelation('activityMaps');
+    cloService
+      .get(criteria)
+      .then(res => {
+        setClos(res.data.sort((a, b) => a.number - b.number));
+      })
+      .catch(() => uiService.toastError('Could not fetch clos!'));
+  }, []);
+  return clos;
+};
+
+const useActivities = (allocationId: string) => {
+  const [activities, setActivities] = useState<ActivityType[]>();
+  useEffect(() => {
+    const criteria = new ManyCriteria<ActivityType>();
+    criteria.addCondition('allocation', allocationId);
+    criteria.addRelation('evaluations');
+    activityService
+      .get(criteria)
+      .then(res => {
+        setActivities(res.data);
+      })
+      .catch(() => uiService.toastError('Could not fetch activities!'));
+  }, []);
+  return activities;
+};
 
 export default function FCARScreen() {
   const route = useRoute<any>();
   const allocation: AllocationType = route.params!.allocation;
+  const clos = useClos(allocation.course!.id);
+  const activities = useActivities(allocation.id);
+
   return (
     <ScrollView>
-      <Card>
+      <Card style={{margin: 8}}>
         <Caption style={{textAlign: 'center'}}>
           Faculty Course Assessment Report
         </Caption>
-        <Title style={{textAlign: 'center'}}>{allocation.section!.id}</Title>
+        <Title style={{textAlign: 'center', fontWeight: 'bold'}}>
+          {allocation.section!.id}
+        </Title>
         <Text style={{textAlign: 'center'}}>{allocation.course!.title}</Text>
+        {clos && activities ? (
+          clos.map(c => (
+            <>
+              <Divider style={{marginTop: 12}} />
+              <Caption style={{margin: 8, textAlign: 'center'}}>
+                CLO {c.number}
+              </Caption>
+              <Card style={{margin: 8, overflow: 'hidden'}} mode="outlined">
+                <FCARTable
+                  activities={activities.filter(a =>
+                    c.activityMaps.some(m => m.activity.id === a.id),
+                  )}
+                />
+              </Card>
+            </>
+          ))
+        ) : (
+          <ActivityIndicator style={{margin: 16, alignSelf: 'center'}} />
+        )}
       </Card>
     </ScrollView>
   );
