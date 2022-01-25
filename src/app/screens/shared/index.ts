9b665f9f098cde77ffe1d5346db856cd73809a2e
Osama Khan <Osama6756@gmail.com>
2022-01-25 23:32:56 +0500

    Improved screen exports

--- src/app/screens/shared/index.ts
+++ src/app/screens/shared/index.ts
@@ -1,4 +1,4 @@
 export {StudentResultsScreen} from './student-results.screen';
-export {EvaluationDetailScreen} from './evaluation-detail';
-export {ManageAssessmentScreen} from './assessment';
+export * from './evaluation-detail';
 export * from './transcript';
+export * from './assessment';
