850d2f517d1e87cb5ad53c6afb7b8e86a71a01f5
Osama Khan <Osama6756@gmail.com>
2022-01-30 14:39:36 +0500

    Optimized update on HOD Sections Table

--- src/app/screens/hod/sections/sections.screen.tsx
+++ src/app/screens/hod/sections/sections.screen.tsx
@@ -1,51 +1,66 @@
-import {FetchingDataTable} from '@app/components/data-table';
+import {DataTable} from '@app/components/data-table';
 import {ManyCriteria} from '@app/models/criteria';
 import {sectionDetailRoute} from '@app/routes/hod.routes';
 import sectionService from '@app/services/section.service';
 import uiService from '@app/services/ui.service';
 import {SectionType} from '@app/types';
 import {useNavigation} from '@react-navigation/native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView} from 'react-native';
-import {Card, FAB, Text} from 'react-native-paper';
+import {ActivityIndicator, Card, FAB, Text} from 'react-native-paper';
 import AddSectionModal from './add-section.modal';
 
 export default function SectionsScreen() {
   const navigation = useNavigation<any>();
-  const [updates, setUpdates] = useState(0);
+  const [sections, setSections] = useState<SectionType[]>();
   const [shown, setShown] = useState(false);
   const hideModal = () => setShown(false);
+
+  useEffect(() => {
+    const crit = new ManyCriteria<SectionType>({relations: ['program']});
+    sectionService
+      .get(crit)
+      .then(r => {
+        setSections(r.data);
+      })
+      .catch(() => uiService.toastError('Could not fetch sections!'));
+  }, []);
+
   return (
     <>
       <ScrollView>
         <Card style={{margin: 16, marginBottom: 96}}>
-          <FetchingDataTable
-            key={updates}
-            criteria={new ManyCriteria<SectionType>({relations: ['program']})}
-            fetchMethod={c => sectionService.get(c)}
-            rowOnPress={section => {
-              navigation.navigate(sectionDetailRoute.name, {section});
-            }}
-            columns={[
-              {
-                title: 'Program',
-                selector: ({item}) => <Text>{item.program!.title}</Text>,
-              },
-              {
-                title: 'Semester',
-                selector: 'semester',
-              },
-              {
-                title: 'Name',
-                selector: 'name',
-              },
-            ]}
-          />
+          {sections ? (
+            <DataTable
+              data={sections}
+              rowOnPress={section => {
+                navigation.navigate(sectionDetailRoute.name, {section});
+              }}
+              columns={[
+                {
+                  title: 'Program',
+                  selector: ({item}) => <Text>{item.program!.title}</Text>,
+                },
+                {
+                  title: 'Semester',
+                  selector: 'semester',
+                },
+                {
+                  title: 'Name',
+                  selector: 'name',
+                },
+              ]}
+            />
+          ) : (
+            <ActivityIndicator style={{margin: 16, alignSelf: 'center'}} />
+          )}
         </Card>
       </ScrollView>
       <FAB
         icon="plus"
         style={{position: 'absolute', bottom: 16, right: 16}}
+        disabled={!sections}
+        loading={!sections}
         onPress={() => {
           setShown(true);
         }}
@@ -55,8 +70,8 @@ export default function SectionsScreen() {
           hideModal();
           sectionService
             .insert(data)
-            .then(() => {
-              setUpdates(updates + 1);
+            .then(s => {
+              setSections([...sections!, s.data]);
               uiService.toastSuccess('Successfully added Section!');
             })
             .catch(() => uiService.toastError('Could not add Section!'));
