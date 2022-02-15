34d2c8d7e5a701c7118f17cbb4b689bc87746ed7
Osama Khan <Osama6756@gmail.com>
2022-02-15 19:28:14 +0500

    Updated Login screen header title

--- src/app/routes/auth.routes.ts
+++ src/app/routes/auth.routes.ts
@@ -4,6 +4,9 @@ import {RouteType} from '@app/types/route.type';
 export const loginRoute: RouteType = {
   id: 'login',
   name: 'Login',
+  options: {
+    headerTitle: 'BIIT OBE',
+  },
   component: LoginScreen,
 };
 
